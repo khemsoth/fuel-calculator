8b07e0c1527507b5ca9bcfc5470a0509d958ca19
khemsoth <kevhemsoth@gmail.com>
2020-11-24 18:44:50 -0500

    added new font, calculator: styled button, all: adjusted responsiveness

--- src/components/Calculator.vue
+++ src/components/Calculator.vue
@@ -1,33 +1,36 @@
 <template>
   <div class="flex flex-column justify-center"> 
-    <main class="">
+    <main class="w-full">
       <form class="flex flex-col p-6 w-full bg-custom-blue">
-        <div class="my-5">
-          <label class="text-left text-custom-tan" for="distance">Distance: </label>
-          <input  class="w-full text-center mb-4" v-model="distance" type="number" name="distance"> 
-          <input v-model="selected" type="radio" name="laps" value="laps"> <label class="text-custom-tan mr-6" for="laps">Laps</label>
-          <input v-model="selected" type="radio" name="time" value="time"> <label class="text-custom-tan" for="time">Time <span class="italic text-sm text-custom-tan">(minutes)</span></label>
+        <div class="flex flex-col lg:w-3/4 lg:mx-auto">
+          <div class="my-5">
+            <label class="text-left text-custom-tan" for="distance">Distance: </label>
+            <input  class="w-full text-center mb-4" v-model="distance" type="number" name="distance"> 
+            <input v-model="selected" type="radio" name="laps" value="laps"> <label class="text-custom-tan mr-6" for="laps">Laps</label>
+            <input v-model="selected" type="radio" name="time" value="time"> <label class="text-custom-tan" for="time">Time <span class="italic text-sm text-custom-tan">(minutes)</span></label>
+          </div>
+          <div class="my-5">
+            <label class="text-custom-tan" for="fuel-con">Average Fuel Consumption: <span class="italic text-sm text-custom-tan">(per lap)</span> </label>
+            <input class="w-full text-center mb-4" v-model="fuelConsumption" type="number"> 
+            <input v-model="fluidSelected" type="radio" name="liters" value="liters"> <label class="text-custom-tan mr-6" for="liters">Liters</label>
+            <input v-model="fluidSelected" type="radio" name="gallons" value="gallons"> <label class="text-custom-tan" for="gallons">Gallons</label>
+          </div>
+          <div class="my-5" v-bind:style='{ display: laptimeVisible }'>
+            <label class="text-custom-tan" for="lap-time">Average Lap Time: </label>
+            <div class="w-full text-custom-tan"><input class="w-1/4 text-center" type="number" v-model="lapTimeMin"> : <input class="w-1/4 text-center" type="number" v-model="lapTimeSec"> . <input class="w-1/4 text-center" type="number" v-model="lapTimeMS"> (MM:SS.sss)</div> 
+          </div>
+          <div class="my-5">
+            <label class="text-custom-tan" for="extra-fuel">How many laps of extra fuel do you want? </label>
+            <input class="w-full text-center" v-model="extraFuel" type="number" name="extra-fuel">
+          </div>
+          <input class="calc-btn p-2 w-2/5 self-end rounded-lg bg-white text-custom-blue hover:bg-custom-brown hover:border-custom-tan hover: border hover:text-custom-tan" v-on:click="calculate" type="button" value="Calculate!">
         </div>
-        <div class="my-5">
-          <label class="text-custom-tan" for="fuel-con">Average Fuel Consumption: <span class="italic text-sm text-custom-tan">(per lap)</span> </label>
-          <input class="w-full text-center mb-4" v-model="fuelConsumption" type="number"> 
-          <input v-model="fluidSelected" type="radio" name="liters" value="liters"> <label class="text-custom-tan mr-6" for="liters">Liters</label>
-          <input v-model="fluidSelected" type="radio" name="gallons" value="gallons"> <label class="text-custom-tan" for="gallons">Gallons</label>
-        </div>
-        <div class="my-5" v-bind:style='{ display: laptimeVisible }'>
-          <label class="text-custom-tan" for="lap-time">Average Lap Time: </label>
-          <div class="w-full text-custom-tan"><input class="w-1/4 text-center" type="number" v-model="lapTimeMin"> : <input class="w-1/4 text-center" type="number" v-model="lapTimeSec"> . <input class="w-1/4 text-center" type="number" v-model="lapTimeMS"> (MM:SS.sss)</div> 
-        </div>
-        <div class="my-5">
-          <label class="text-custom-tan" for="extra-fuel">How many laps of extra fuel do you want? </label>
-          <input class="w-full text-center" v-model="extraFuel" type="number" name="extra-fuel">
-        </div>
-        <input class="p-2 w-2/3 self-end" v-on:click="calculate" type="button" value="Calculate!">
         <p class="text-custom-tan italic mt-4">All results are estimates. Your mileage may vary.</p>
       </form>
-      <aside v-bind:style='{ visibility: isVisible }' class="m-8 p-6 border border-custom-orange rounded-lg">
+      <aside v-bind:style='{ visibility: isVisible }' class="m-8 p-6 border border-custom-orange rounded-lg lg:w-3/4 lg:mx-auto">
         <p>You need {{ this.result }} {{ this.fluidSelected }} of fuel.</p>
       </aside>
+      
     </main>
   </div>
 </template>
@@ -136,6 +139,10 @@ input[type=number] {
 
   * {
     color: #5d4c46;
+  }
+
+  .calc-btn:hover {
+    cursor: pointer;
   }
 
 </style>
